8d77488cf1dd85b11a7f83d970782314914ca694
bebe228 <Andriu.botok@gmail.com>
2024-10-25 22:09:33 +0300

    relese 25.10.2024 22:10

--- Lab_11_AT/Lab_11_AT.cpp
+++ Lab_11_AT/Lab_11_AT.cpp
@@ -3,110 +3,183 @@
 using namespace std;
 
 struct Node {
-    int data;
-    Node* next;
-    Node* prev;
+	int data;
+	Node* next;
+	Node* prev;
 
-    Node(int data) : data(data), next(nullptr), prev(nullptr) {}
+	Node(int data) : data(data), next(nullptr), prev(nullptr) {}
 };
 
-
-void forwardTraversal(Node* head)// Функція для прямого обходу списку 
-{
-    Node* curr = head;
-    while (curr != nullptr) {
-        cout << curr->data << " ";
-        curr = curr->next;
-    }
-    cout << endl;
+void forwardTraversal(Node* head) {
+	Node* curr = head;
+	while (curr != nullptr) {
+		cout << curr->data << " ";
+		curr = curr->next;
+	}
+	cout << endl;
 }
-
 
 void backwardTraversal(Node* head) {
-    
-    Node* tail = head;
-    while (tail->next != nullptr) {
-        tail = tail->next;
-    }
-    Node* curr = tail;
-    while (curr != nullptr) {
-        cout << curr->data << " ";
-        curr = curr->prev;
-    }
-    cout << endl;
+	if (head == nullptr) return;
+
+	Node* tail = head;
+	while (tail->next != nullptr) {
+		tail = tail->next;
+	}
+
+	Node* curr = tail;
+	while (curr != nullptr) {
+		cout << curr->data << " ";
+		curr = curr->prev;
+	}
+	cout << endl;
 }
 
-void insertHead(Node* head, int data)
-{
-    Node* tail = head;
-    while (tail->next != nullptr) {
-        tail = tail->next;
-    }
-    Node* curr = tail;
-    while (curr != nullptr) {
-        cout << curr->data << " ";
-        curr = curr->prev;
-    }
-    cout << endl;
+Node* insertBegin(Node* head, int data) {
+	Node* new_node = new Node(data);
+	new_node->next = head;
+	if (head != nullptr) {
+		head->prev = new_node;
+	}
+	return new_node;
 }
-
 
 Node* insertEnd(Node* head, int data) {
-    Node* new_node = new Node(data);
-    if (head == nullptr) {
-        return new_node;
-    }
-    Node* curr = head;
-    while (curr->next != nullptr) {
-        curr = curr->next;
-    }
-    curr->next = new_node;
-    new_node->prev = curr;
-    return head;
+	Node* new_node = new Node(data);
+	if (head == nullptr) {
+		return new_node;
+	}
+	Node* curr = head;
+	while (curr->next != nullptr) {
+		curr = curr->next;
+	}
+	curr->next = new_node;
+	new_node->prev = curr;
+	return head;
 }
 
-void printList(Node* head) 
-{
-    Node* curr = head;
-    while (curr != nullptr) {
-        cout << curr->data << " ";
-        curr = curr->next;
-    }
-    cout << "\n";
+Node* deleteFirst(Node* head) {
+	if (head == nullptr) {
+		return nullptr;
+	}
+	Node* temp = head;
+	head = head->next;
+	if (head != nullptr) {
+		head->prev = nullptr;
+	}
+	delete temp;
+	return head;
+}
+
+
+Node* deleteLast(Node* head) {
+	if (head == nullptr) {
+		return nullptr;
+	}
+	if (head->next == nullptr) {
+		delete head;
+		return nullptr;
+	}
+	Node* curr = head;
+	while (curr->next != nullptr) {
+		curr = curr->next;
+	}
+	curr->prev->next = nullptr;
+	delete curr;
+	return head;
+}
+
+Node* deleteMiddle(Node* head) {
+	if (head == nullptr || head->next == nullptr) {
+		return head;
+	}
+
+	Node* slow = head;
+	Node* fast = head;
+	while (fast != nullptr && fast->next != nullptr) {
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	if (slow->prev != nullptr) {
+		slow->prev->next = slow->next;
+	}
+	if (slow->next != nullptr) {
+		slow->next->prev = slow->prev;
+	}
+	delete slow;
+	return head;
+}
+
+void printList(Node* head) {
+	Node* curr = head;
+	
+	while (curr != nullptr) {
+		cout << curr->data << " ";
+		curr = curr->next;
+		
+	}
+	cout << " \n" ;
+
 }
 
 int main() {
-    
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
-    Node* head = new Node(2);
-    Node* temp1 = new Node(3);
-    Node* temp2 = new Node(4);
-    head->next = temp1;
-    temp1->prev = head;
-    temp1->next = temp2;
-    temp2->prev = temp1;
+	Node* head = new Node(2);
+	head = insertEnd(head, 3);
+	head = insertEnd(head, 4);
+	head = insertEnd(head, 5);
 
-   
-    cout << "Друк оригінального списку ";
-    printList(head);
 
-    
-    head = insertEnd(head, 5);
-    head = insertHead(head, 1);
-    
-    cout << "Додавання одного елемента в список ";
-    printList(head);
 
-    
-    cout << "Прямий обхід списку ";
-    forwardTraversal(head);
+	cout << "Друк оригінального списку		";
+	printList(head);
 
-    
-    Node* tail = temp2;
-    cout << "Обратний обхід списку ";
-    backwardTraversal(head);
 
-    return 0;
+
+	cout << "Додано останій елемент			";
+	head = insertEnd(head, 6);
+	printList(head);
+
+
+
+	cout << "Додано перший елемент			";
+	head = insertBegin(head, 1);
+	printList(head);
+
+
+
+	cout << "Прямий обхід списку			";
+	forwardTraversal(head);
+
+
+
+	cout << "Зворотний обхід списку			";
+	backwardTraversal(head);
+
+
+
+	head = deleteFirst(head);
+	cout << "Видалення першого елемента		";
+	printList(head);
+
+
+
+	head = deleteLast(head);
+	cout << "Видалення останнього елемента		";
+	printList(head);
+
+
+
+	head = deleteMiddle(head);
+	cout << "Видалення середнього елемента		";
+	printList(head);
+
+
+
+
+
+	return 0;
 }
